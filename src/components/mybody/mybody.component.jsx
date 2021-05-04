066892d459ad29a2a1a9948744b2d13e397df74f
Brianna Shade <bri.krystalle@gmail.com>
2021-05-04 14:10:29 -0400

    images

--- src/components/mybody/mybody.component.jsx
+++ src/components/mybody/mybody.component.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 import Image from 'react-bootstrap/Image'
-import "./body.style.css";
 
+import dailyplanner from './dailyplanner.jpg'
+import jquiz from './jquiz.jpg'
+import budget from './budget.jpg'
+import notetaker from './notetaker.jpg'
+import workoutapp from './workouttrack.jpg'
+import employeedirectory from './employee.jpg'
+
+
+import "./body.style.css";
 
 
 const MyBody = () => {
@@ -11,20 +19,16 @@ const MyBody = () => {
 
             <div class="col-sm-12 my-auto">
                 <h1>Budget Tracker</h1>
-                <Image src="holder.js/171x180" roundedCircle />
-             
+                <img src={budget} class="img-thumbnail" alt="image"/>
                 <a href="https://budget-tracker-pgwd.herokuapp.com/">Budget Tracker Application</a>
                 <p>Description</p>
                 <a href="https://github.com/pgwdbri/BudgetTracker">Check out the Github Repository for this project.</a>
-                
-
              </div>
 
              <div class="col-sm-12 my-auto">
 
                 <h1>Fitness Tracker</h1>
-                <Image src="holder.js/171x180" roundedCircle />
-               
+                <img src={workoutapp} class="img-thumbnail" alt="image" />
                 <a href="https://fitness-tracker-pgwd.herokuapp.com/">Fitness Tracker Application</a>
                 <p>Description</p>
                 <a href="https://github.com/pgwdbri/FitnessTracker">Check out the Github Repository for this project.</a>
@@ -33,8 +37,7 @@ const MyBody = () => {
 
              <div class="col-sm-12 my-auto">
                 <h1>Daily Planner</h1>
-                <Image src="holder.js/171x180" roundedCircle />
-                
+                <img src={dailyplanner} class="img-thumbnail" alt="image" />
                 <a href="https://pgwdbri.github.io/DailyPlanner/">Daily Planner Application</a>
                 <p>Description</p>
                 <a href="https://github.com/pgwdbri/DailyPlanner">Check out the Github Repository for this project.</a>
@@ -44,7 +47,7 @@ const MyBody = () => {
              <div class="col-sm-12 my-auto">
 
                 <h1>Note Taker</h1>
-                <Image src="holder.js/171x180" roundedCircle />
+                <img src={notetaker} class="img-thumbnail" alt="image" />
                 <a href="https://note-taker-pgwdbri.herokuapp.com/">Note Taker Application</a>
                 <p>Description</p>
                 <a href="https://github.com/pgwdbri/NoteTaker">Check out the Github Repository for this project.</a>
@@ -54,7 +57,7 @@ const MyBody = () => {
              <div class="col-sm-12 my-auto">
 
                 <h1>Employee Directory</h1>
-                <Image src="holder.js/171x180" roundedCircle />
+                <img src={employeedirectory} class="img-thumbnail" alt="image" />
                 <a href="https://employee-directory-pgwdb.herokuapp.com/">Employee Directory Application</a>
                 <p>Description</p>
                 <a href="https://github.com/pgwdbri/userdirectory">Check out the Github Repository for this project.</a>
@@ -64,8 +67,7 @@ const MyBody = () => {
              <div class="col-sm-12 my-auto">
 
                 <h1>JavaScript Quiz</h1> 
-                <Image src="holder.js/171x180" roundedCircle />
-                
+                <img src={jquiz} class="img-thumbnail"alt="image" />
                 <a href="https://pgwdbri.github.io/JavaScriptQuiz/">JavaScript Quiz Application</a>
                 <p>Description</p>
                 <a href="https://github.com/pgwdbri/JavaScriptQuiz">Check out the Github Repository for this project.</a>
