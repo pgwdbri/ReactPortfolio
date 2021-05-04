da54d584b9852ea5ef788d6b97afcbfe3d183e1f
Brianna Shade <bri.krystalle@gmail.com>
2021-05-04 13:26:55 -0400

    links to projects

--- src/components/mybody/mybody.component.jsx
+++ src/components/mybody/mybody.component.jsx
@@ -10,39 +10,66 @@ const MyBody = () => {
         <div class="text-center">
 
             <div class="col-sm-12 my-auto">
+                <h1>Budget Tracker</h1>
                 <Image src="holder.js/171x180" roundedCircle />
-                <h1>Project 1</h1>
-                <p>This project is an application</p>
+             
+                <a href="https://budget-tracker-pgwd.herokuapp.com/">Budget Tracker Application</a>
+                <p>Description</p>
+                <a href="https://github.com/pgwdbri/BudgetTracker">Check out the Github Repository for this project.</a>
+                
+
              </div>
 
              <div class="col-sm-12 my-auto">
+
+                <h1>Fitness Tracker</h1>
                 <Image src="holder.js/171x180" roundedCircle />
-                <h1>Project 2</h1>
-                <p>This project is an application</p>
+               
+                <a href="https://fitness-tracker-pgwd.herokuapp.com/">Fitness Tracker Application</a>
+                <p>Description</p>
+                <a href="https://github.com/pgwdbri/FitnessTracker">Check out the Github Repository for this project.</a>
+                
              </div> 
 
              <div class="col-sm-12 my-auto">
+                <h1>Daily Planner</h1>
                 <Image src="holder.js/171x180" roundedCircle />
-                <h1>Project 3</h1>
-                <p>This project is an application</p>
+                
+                <a href="https://pgwdbri.github.io/DailyPlanner/">Daily Planner Application</a>
+                <p>Description</p>
+                <a href="https://github.com/pgwdbri/DailyPlanner">Check out the Github Repository for this project.</a>
+                
              </div>
 
              <div class="col-sm-12 my-auto">
+
+                <h1>Note Taker</h1>
                 <Image src="holder.js/171x180" roundedCircle />
-                <h1>Project 4</h1>
-                <p>This project is an application</p>
+                <a href="https://note-taker-pgwdbri.herokuapp.com/">Note Taker Application</a>
+                <p>Description</p>
+                <a href="https://github.com/pgwdbri/NoteTaker">Check out the Github Repository for this project.</a>
+                
              </div>
 
              <div class="col-sm-12 my-auto">
+
+                <h1>Employee Directory</h1>
                 <Image src="holder.js/171x180" roundedCircle />
-                <h1>Project 5</h1>
-                <p>This project is an application</p>
+                <a href="https://employee-directory-pgwdb.herokuapp.com/">Employee Directory Application</a>
+                <p>Description</p>
+                <a href="https://github.com/pgwdbri/userdirectory">Check out the Github Repository for this project.</a>
+                
              </div>
 
              <div class="col-sm-12 my-auto">
+
+                <h1>JavaScript Quiz</h1> 
                 <Image src="holder.js/171x180" roundedCircle />
-                <h1>Project 6</h1>
-                <p>This project is an application</p>
+                
+                <a href="https://pgwdbri.github.io/JavaScriptQuiz/">JavaScript Quiz Application</a>
+                <p>Description</p>
+                <a href="https://github.com/pgwdbri/JavaScriptQuiz">Check out the Github Repository for this project.</a>
+                
              </div>
 
 
