282183f9dfaadb4d14ed052b564803f07fbe4606
Brianna Shade <bri.krystalle@gmail.com>
2021-05-04 03:22:31 -0400

    footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import react from 'react';
 //components
 import MyNavbar from "./components/mynavbar/mynavbar.component";
 import './App.css';
+import FooterPanel from './components/mynavbar/myfooter.component';
 
 const App = () => {
   return (<div>
     <MyNavbar />
 
+    
 
+    <FooterPanel />
       My Portfolio: </div>);
 }
 
