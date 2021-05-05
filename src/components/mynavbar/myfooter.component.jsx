f8f76fb241148a0561d0c8d6bf33bf21273dd507
Brianna Shade <bri.krystalle@gmail.com>
2021-05-05 18:59:59 -0400

    deploy

--- src/components/mynavbar/myfooter.component.jsx
+++ src/components/mynavbar/myfooter.component.jsx
@@ -6,7 +6,7 @@ const FooterPanel = () => {
   return (
     <div>
       <div className="py-2 text-center footer-style"> Contact Information </div>
-      <div className="py-2 text-center"> Brianna Shade   bri.krystalle@gmail.com   ###-###-#### </div>
+      <div className="py-2 text-center"> Brianna Shade ~ bri.krystalle@gmail.com ~ ###-###-#### </div>
     </div>
   );
 };
